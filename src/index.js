305ace8a37459d5a88cfbe0726a50a34599c3541
zhangyunnian <zhangyunnian@bytedance.com>
2023-04-06 00:13:58 +0800

    fix: init

---
+++ src/index.js
@@ -0,0 +1,53 @@
+import path from 'path';
+import Koa from 'koa';
+import Router from 'koa-router';
+import views from 'koa-views';
+import { ChatGPTAPI } from 'chatgpt';
+
+const __dirname = path.dirname(new URL(import.meta.url).pathname);
+
+const run = () => {
+  let parentMessageId;
+  const gptBot = new ChatGPTAPI({
+    apiKey: process.env.OPENAI_API_KEY,
+  });
+
+  const app = new Koa();
+  const router = new Router();
+
+  app.use(
+    views(__dirname + '/views', {
+      extension: 'ejs',
+    })
+  );
+
+  // 路由定义
+  router.get('/', async (ctx, next) => {
+    const q = (ctx.request.query.q || '').trim();
+    if (!q) return next();
+
+    try {
+      const res = await gptBot.sendMessage(q, { parentMessageId });
+      parentMessageId = res.id;
+      await ctx.render('index', {
+        title: 'zl_gpt',
+        message: res.text,
+      });
+    } catch (err) {
+      ctx.body = err.message;
+      ctx.status = 500;
+    }
+
+    next();
+  });
+
+  app.use(router.routes());
+
+  app.listen(process.env.PORT, () =>
+    console.log(`http://localhost:${process.env.PORT}`)
+  );
+};
+
+export default {
+  run,
+};
